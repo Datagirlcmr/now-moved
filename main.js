1775827c7cff901ffe5912a8ecb504988aef34bd
datagirl <datagirlcmr@gmail.com>
2020-09-08 20:37:40 +0100

    update selma.skills

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let qualities = ['Full-Stack Developer', 'M.B.A. and Finance Expert', 'Open to freelance software Development'];
 let count=0;
 let t = setInterval(function () {
-    var ele = document.getElementById('blink');
+    let ele = document.getElementById('blink');
     
     ele.innerHTML = qualities[count++];
     
@@ -9,3 +9,18 @@ let t = setInterval(function () {
       count=0;
   
 }, 1000);
+
+let skills = [" HTML " , " CSS " , " Bootstrap " , "JavaScript" , " Ruby " , " Ruby on Rails " , " React " , " git "]
+let counter = 0;
+
+const animate =() => {
+  const selmaSkills = document.querySelector('#skills')
+  selmaSkills.innerHTML = skills[counter++]
+
+  if (counter === skills.length) {
+    counter=0;
+  }
+
+}
+
+let animation = setInterval(animate, 1000)
